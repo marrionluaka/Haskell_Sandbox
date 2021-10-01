7666eb3dd90d24a53da10e5c2734003d75b96e23
marrionluaka <marrion91300@gmail.com>
2021-09-30 19:32:51 -0700

    Adding foldl

--- src/map_filter_foldl.hs
+++ src/map_filter_foldl.hs
@@ -8,8 +8,23 @@ myFilter test (x:xs) = if test x
                        else myFilter test xs
 -- myFilter (\(x:xs) -> x == 'a') ["apple", "banana", "avocado"] => ["apple", "avocado"]
 
+myFoldl f init [] = init
+myFoldl f init (x:xs) = myFoldl f newInit xs
+  where newInit = f init x
+-- myFoldl (+) 0 [1,2,5] => 8
+
 remove test [] = []
 remove test (x:xs) = if test x
                      then remove test xs
                      else x:remove test xs
 -- remove (\x -> x ^ 2 > 10) [1,3,4,5,6,7] => [1,3]
+
+myProduct xs = foldl (*) 1 xs
+-- myProduct [1,2,3,5] => 30
+
+sumOfSquares xs = foldl (+) 0 (map (^2) xs)
+-- sumOfSquares [2,4,6] => 56
+
+rcons acc value = value:acc
+myReverse xs = foldl rcons [] xs
+-- myReverse [1,2,3] => [3,2,1]
