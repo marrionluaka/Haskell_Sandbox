b78366bf9cd08a0b5c4d909e8e98428de7e12882
Marrion Lulu <marrion91300@gmail.com>
2021-12-02 21:21:03 -0800

    Adding storeItem

--- src/sum_types.hs
+++ src/sum_types.hs
@@ -11,5 +11,38 @@ data Artist = Person Name | Band String
 
 data Creator = AuthorCreator Author | ArtistCreator Artist
 
+-- Usage
 hpLovecraft :: Creator
 hpLovecraft = AuthorCreator (Author (TwoInitialsWithLastName 'H' 'P' "Lovecraft"))
+
+
+
+--------------
+data Book = Book {
+  author      :: Creator,
+  isbn        :: String,
+  bookTitle   :: String,
+  bookYear    :: Int,
+  bookPrice   :: Double
+}
+
+data VinylRecord = VinylRecord {
+  artist        :: Creator,
+  recordTitle   :: String,
+  recordYear    :: Int,
+  recordPrice   :: Double
+}
+
+data CollectibleToy = CollectibleToy {
+  name          :: String,
+  description   :: String,
+  toyPrice      :: Double
+}
+
+data StoreItem = BookItem Book | RecordItem VinylRecord | ToyItem CollectibleToy
+
+-- Usage
+price :: StoreItem -> Double
+price (BookItem book) = bookPrice book
+price (RecordItem record) = recordPrice record
+price (ToyItem toy) = toyPrice toy
