bc0a2fe84d981262b42fd4716840c52027648b5a
Marrion Lulu <marrion91300@gmail.com>
2021-12-09 20:38:25 -0800

    Adding maybe type

--- src/parameterized_types.hs
+++ src/parameterized_types.hs
@@ -1,4 +1,5 @@
 import qualified Data.Map as Map
+import Data.List (intercalate)
 
 data Organ = Heart | Brain | Kidney | Spleen deriving (Show, Eq)
 
@@ -17,3 +18,39 @@ organCatalog = Map.fromList organPairs
 -- Usage
 -- GHCi> Map.lookup 7 organCatalog
 -- Just Heart
+
+
+
+--------------
+possibleDrawers :: [Int]
+possibleDrawers = [1 .. 50]
+
+getDrawerContents :: [Int] -> Map.Map Int Organ -> [Maybe Organ]
+getDrawerContents ids catalog = map getContents ids
+  where getContents = (`Map.lookup` catalog)
+
+availableOrgans :: [Maybe Organ]
+availableOrgans = getDrawerContents possibleDrawers organCatalog
+
+countOrgan :: Organ -> [Maybe Organ] -> Int
+countOrgan organ available = length (filter (\x -> x == Just organ) available)
+
+
+
+--------------
+isSomething :: Maybe Organ -> Bool
+isSomething Nothing = False
+isSomething (Just _) = True
+
+justTheOrgan :: [Maybe Organ]
+justTheOrgan = filter isSomething availableOrgans
+
+showOrgan :: Maybe Organ -> String
+showOrgan (Just organ) = show organ
+showOrgan Nothing = ""
+
+organList :: [String]
+organList = map showOrgan justTheOrgan
+
+cleanList :: String
+cleanList = intercalate "," organList
