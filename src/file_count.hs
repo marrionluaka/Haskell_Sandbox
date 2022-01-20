43381124f166486f271173b5d6d2293b9d6b9112
Marrion Lulu <marrion91300@gmail.com>
2022-01-20 08:32:16 -0800

    Adding file count

---
+++ src/file_count.hs
@@ -0,0 +1,23 @@
+import System.Environment
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let fileName = head args
+  input <- readFile fileName
+  let summary = (countsText . getCounts) input
+  appendFile "stats.dat" (mconcat [fileName, " ", summary, "\n"])
+  putStrLn summary
+
+getCounts :: String -> (Int, Int, Int)
+getCounts input = (charCount, wordCount, lineCount)
+  where charCount = length input
+        wordCount = (length . words) input
+        lineCount = (length . lines) input
+
+countsText :: (Int, Int, Int) -> String
+countsText (cc, wc, lc) = unwords ["chars: ", show cc, " words: ", show wc, " lines: ", show lc]
+
+
+-- Usage
+-- ghci> :main "cheat-sheet.txt"
