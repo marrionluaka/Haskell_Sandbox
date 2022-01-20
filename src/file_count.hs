5e6cf63260448a523bc2547aaeaa3be5f2874afa
Marrion Lulu <marrion91300@gmail.com>
2022-01-20 09:01:14 -0800

    Refactoring file count to use Text

--- src/file_count.hs
+++ src/file_count.hs
@@ -1,22 +1,28 @@
+-- Data.Text is a strict data type (it doesn't use lazy evaluation)
+{-# LANGUAGE OverloadedStrings #-}
+import System.IO
 import System.Environment
+import qualified Data.Text as T
+import qualified Data.Text.IO as TI
 
 main :: IO ()
 main = do
   args <- getArgs
   let fileName = head args
-  input <- readFile fileName
+  input <- TI.readFile fileName
+
   let summary = (countsText . getCounts) input
-  appendFile "stats.dat" (mconcat [fileName, " ", summary, "\n"])
-  putStrLn summary
+  TI.appendFile "stats.dat" (mconcat [T.pack(fileName), " ", summary, "\n"])
+  TI.putStrLn summary
 
-getCounts :: String -> (Int, Int, Int)
+getCounts :: T.Text -> (Int, Int, Int)
 getCounts input = (charCount, wordCount, lineCount)
-  where charCount = length input
-        wordCount = (length . words) input
-        lineCount = (length . lines) input
+  where charCount = T.length input
+        wordCount = (length . T.words) input
+        lineCount = (length . T.lines) input
 
-countsText :: (Int, Int, Int) -> String
-countsText (cc, wc, lc) = unwords ["chars: ", show cc, " words: ", show wc, " lines: ", show lc]
+countsText :: (Int, Int, Int) -> T.Text
+countsText (cc, wc, lc) = T.pack (unwords ["chars: ", show cc, " words: ", show wc, " lines: ", show lc])
 
 
 -- Usage
